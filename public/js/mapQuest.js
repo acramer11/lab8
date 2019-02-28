21c3ebba20413f55e6cf668110819087f956843a
Avi Cramer <abcramer@ucsd.edu>
2019-02-28 12:46:00 -0800

    finished lab8

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,14 @@
 function initMap() {
 	// add your code here
+	L.mapquest.key = 'AbyLMFMmhHcrLqDsC7xddg67BPQTYuzv';
+
+// 'map' refers to a <div> element with the ID map
+var map = L.mapquest.map('map', {
+  center: [32.8712192, -117.2267008],
+  layers: L.mapquest.tileLayer('map'),
+  zoom: 12,
+  zoomControl: false
+});
+
+	L.marker([32.8712192, -117.2267008]).addTo(map);
 }
